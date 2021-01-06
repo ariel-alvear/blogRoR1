32518c4130a612a5e523f120aad76e5d5839f12a
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-01-05 21:14:47 -0300

    requerimientos listos

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
     validates :title, :image_url, :content, presence: true
+
+    before_save :erase_word
+
+    def replace_word
+        self.content = self.content.uppercase.gsub "SPOILER", ""
+    end
 end
